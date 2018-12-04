b239fe14846e317470fd80658a7b9599cd2d7b4b
niyazm524 <niyazm524@yandex.ru>
2018-12-04 10:50:36 +0300

    Added IntArrayList

---
+++ src/ml/bimdev/lists/IntList.java
@@ -0,0 +1,14 @@
+package ml.bimdev.lists;
+
+interface IntList {
+    int get(int index);
+    int size();
+    void add(int i);
+    boolean contains(int value);
+    void remove(int index);
+	Iterator iterator();
+    int[] toArray();
+    void addAll(IntList list);
+    void addAll(IntList list, int offset);
+
+}
---
+++ src/ml/bimdev/lists/Main.java
@@ -0,0 +1,18 @@
+package ml.bimdev.lists;
+
+public class Main {
+
+    public static void main(String[] args) {
+        IntList list = new IntArrayList();
+		list.add(41);
+		list.add(43);
+		list.add(45);
+		list.add(47);
+		
+		Iterator iterator = list.iterator();
+		while(iterator.hasNext()) {
+			int e = iterator.next();
+			System.out.println(e);
+		}
+    }
+}
