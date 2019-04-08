0c86d0a98cc2dfa1f1c9461811ff007454897cdc
niyazm524 <niyazm524@gmail.com>
2019-04-08 11:08:05 +0300

    lesson 31 commented out

--- src/ml/bimdev/lesson31/BruteTask.java
+++ src/ml/bimdev/lesson31/BruteTask.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class BruteTask {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(generateCombinationsRecursion("abc".toCharArray(), )));
+        //System.out.println(Arrays.toString(generateCombinationsRecursion("abc".toCharArray(), )));
     }
 
     String[] generateCombinationsLoop(String input) {
@@ -27,13 +27,13 @@ public class BruteTask {
 
      */
 
-    private static String[] generateCombinationsRecursion(String input) {
-        if(input.length() == 1) return new String[] {input};
-        char first = input.charAt(0);
-        String[] variants = new String[fact(input.length())];
-
-
-    }
+//    private static String[] generateCombinationsRecursion(String input) {
+//        if(input.length() == 1) return new String[] {input};
+//        char first = input.charAt(0);
+//        String[] variants = new String[fact(input.length())];
+//
+//
+//    }
 
     private static int fact(int n) {
         return n == 1 ? 1 : n * fact(n - 1);
